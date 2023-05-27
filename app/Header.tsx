b9103c64ea6f8fcd2b3576d06ba24ff587d419a1
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-05-27 23:17:15 +0200

    customer updates

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <nav className="fixed left-0 right-0 top-0 z-10 flex h-20 flex-row items-center justify-between bg-[#1d1d1d] bg-opacity-70 px-4 py-2 backdrop-blur-lg backdrop-filter md:h-28 md:py-4 ">
+    <nav className="fixed left-0 right-0 top-0 z-10 flex h-20 flex-row items-center justify-between bg-[#f5f5f5] bg-opacity-50 px-4 py-2 backdrop-blur-lg backdrop-filter md:h-28 md:py-4 ">
       <img
         src="https://static.wixstatic.com/media/1752f4_d75b1eda8bd24e9290a20ee4da74e03f~mv2.png/v1/fill/w_285,h_120,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/volvid%20logo.png"
         alt="logo"
