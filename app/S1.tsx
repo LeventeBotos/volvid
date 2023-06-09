5d3be07ceb0f2ac1c676e93ffff9bb60ff1ecf47
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-06-09 10:34:12 +0200

    scroll to form

--- app/S1.tsx
+++ app/S1.tsx
@@ -1,4 +1,54 @@
+"use client";
+
 export const S1 = () => {
+  function onLinkClick(num: number) {
+    const sections = document.getElementsByTagName("form");
+    if (num >= 0 && num < sections.length) {
+      const section = sections[num];
+      const offset = 105; // Adjust this value to set the desired offset
+
+      const sectionTop = section.offsetTop;
+      const bodyScrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      const targetScrollTop = sectionTop - offset;
+
+      // Calculate the scroll distance
+      const scrollDistance = targetScrollTop - bodyScrollTop;
+
+      // Smooth scroll to the target position
+      scrollToSmoothly(scrollDistance);
+    }
+  }
+
+  function scrollToSmoothly(scrollDistance: number, duration = 300) {
+    const start = document.documentElement.scrollTop || document.body.scrollTop;
+    const startTime =
+      "now" in window.performance ? performance.now() : new Date().getTime();
+
+    const animateScroll = function () {
+      const currentTime =
+        "now" in window.performance ? performance.now() : new Date().getTime();
+      const elapsed = currentTime - startTime;
+
+      const ease = function (t: number, b: number, c: number, d: number) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t + b;
+        t--;
+        return (-c / 2) * (t * (t - 2) - 1) + b;
+      };
+
+      window.scroll(0, ease(elapsed, start, scrollDistance, duration));
+
+      if (elapsed < duration) {
+        requestAnimationFrame(animateScroll);
+      } else {
+        window.scroll(0, start + scrollDistance);
+      }
+    };
+
+    animateScroll();
+  }
+
   return (
     <section
       id="hero"
@@ -8,7 +58,12 @@ export const S1 = () => {
       <div className="text-xl">Minőség. Tapasztalat. Megbízhatóság.</div>
       <div>
         {" "}
-        <button className=" items-center rounded-full text-center">
+        <button
+          onClick={() => {
+            onLinkClick(0);
+          }}
+          className=" items-center rounded-full text-center"
+        >
           <span className="rounded-full">Kontakt</span>
         </button>
       </div>
