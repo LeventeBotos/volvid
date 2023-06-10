1b09dd0facd2c280587482c463d716c68640be16
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-06-10 11:36:55 +0200

    firebase

--- app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function POST(req:any, res:any) {
-
-    console.log("hi")
-    // return new Response("hello")
-    // res.status(200).send("Hello")
-    return NextResponse.json("done")
-}
