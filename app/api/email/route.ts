c376bb83d3ba91463922870b9eefb7d33bcd3e41
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-06-12 16:40:00 +0200

    test

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -3,15 +3,25 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SGMAIL);
 
-export async function POST(req: NextRequest) {
-const name = getQSParamFromURL("name", req.url);
-const phone = getQSParamFromURL("phone", req.url);
-const email = getQSParamFromURL("email", req.url);
-const selectedOption = getQSParamFromURL("selectedOption", req.url);
-const selectedOption2 = getQSParamFromURL("selectedOption2", req.url);
-const message = getQSParamFromURL("message", req.url);
+export function getQSParamFromURL(
+  key: string,
+  url: string | undefined
+): string | null {
+  if (!url) return "";
+  const search = new URL(url).search;
+  const urlParams = new URLSearchParams(search);
+  return urlParams.get(key);
+}
 
-const msg = {
+export async function POST(req: NextRequest) {
+  const name = getQSParamFromURL("name", req.url);
+  const phone = getQSParamFromURL("phone", req.url);
+  const email = getQSParamFromURL("email", req.url);
+  const selectedOption = getQSParamFromURL("selectedOption", req.url);
+  const selectedOption2 = getQSParamFromURL("selectedOption2", req.url);
+  const message = getQSParamFromURL("message", req.url);
+
+  const msg = {
     to: 'levinandi98@gmail.com',
     // to: 'volvid.botosk@volvid.hu', // Your email where you'll receive emails
     from: 'botos.levente2007@gmail.com', // Your website email address here
@@ -28,19 +38,10 @@ const msg = {
         Üzenet: ${message}
         </div>`
   };
-  sgMail.send(msg)
-  console.log("sent")
-  console.log(req.url)
-  console.log(msg)
-  return new Response("sent succesfully")
-}
-
-export function getQSParamFromURL(
-  key: string,
-  url: string | undefined
-): string | null {
-  if (!url) return "";
-  const search = new URL(url).search;
-  const urlParams = new URLSearchParams(search);
-  return urlParams.get(key);
+  
+  sgMail.send(msg);
+  console.log("sent");
+  console.log(req.url);
+  console.log(msg);
+  return new NextResponse("sent successfully");
 }
