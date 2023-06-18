9c3eadfa3ea104f6e3bb9f27dd55eb092c5d4fa5
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-06-18 12:14:43 +0200

    api fix

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -3,33 +3,15 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SGMAIL);
 
-// function getQSParamFromURL(
-//   key: string,
-//   url: string | undefined
-// ): string | null {
-//   if (!url) return "";
-//   const search = new URL(url).search;
-//   const urlParams = new URLSearchParams(search);
-//   return urlParams.get(key);
-// }
-
 export async function POST(req: NextRequest) {
-  // let params = new URLSearchParams(document.location.search);
-  // let name = params.get("name"); // is the string "Jonathan"
-  // const name = getQSParamFromURL("name", req.url);
   const { nextUrl: { search } } = req;
   const urlSearchParams = new URLSearchParams(search);
   const params = Object.fromEntries(urlSearchParams.entries());
-  // const phone = getQSParamFromURL("phone", req.url);
-  // const email = getQSParamFromURL("email", req.url);
-  // const selectedOption = getQSParamFromURL("selectedOption", req.url);
-  // const selectedOption2 = getQSParamFromURL("selectedOption2", req.url);
-  // const message = getQSParamFromURL("message", req.url);
 
   const msg = {
     to: 'levinandi98@gmail.com',
     // to: 'volvid.botosk@volvid.hu', // Your email where you'll receive emails
-    from: 'botos.levente2007@gmail.com', // Your website email address here
+    from: 'botos.levente2007@gmail.com', 
     subject: 'Új árajánlat',
     text: 'and easy to do anywhere, even with Node.js',
     html: `<div>${params.name} árajánlatot kért.
@@ -44,9 +26,7 @@ export async function POST(req: NextRequest) {
         </div>`
   };
   
-  sgMail.send(msg)
+  await sgMail.send(msg)
   console.log("sent");
-  console.log(req.url);
-  console.log(params);
   return new NextResponse("sent successfully");
 }
