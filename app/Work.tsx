ea4f940285624eadcd1392c859dc16d12e140cfc
Levente Botos <botos.levente2007@gmail.com>
2023-06-29 17:03:24 +0300

    added work

--- app/Work.tsx
+++ app/Work.tsx
@@ -2,21 +2,44 @@ export const Work = () => {
   return (
     <section className="flex flex-col items-center gap-4">
       <p className="text-2xl font-bold">Dolgozz velünk!</p>
-      <div className="card flex w-full flex-col p-5 text-xl md:w-1/2">
-        <p>
-          Folyamatosan vannak karrierlehetőségek a cégünknél, írj egy{" "}
-          <a
-            className=" hover:underline"
-            href="mailto:volvid.prescherv@volvid.hu"
-          >
-            emailt
-          </a>{" "}
-          vagy keress{" "}
-          <a href="tel:+36 20 403 5353" className=" hover:underline ">
-            telefonon
-          </a>
-          !(Prescher Viktoria)
-        </p>
+      <div className=" flex w-full flex-col gap-3 p-5 text-xl md:w-1/2">
+        <div className="flex justify-between">
+          <img />
+          <p>
+            {" "}
+            {/* Amennyiben egy nagy múltú stabil hátterű magyar cégnél szeretnél
+          dolgozni de kipróbálnád magad akár külföldön is akkor a Volvid
+          számodra remek lehetőség. */}
+            Amennyiben az ország egyik legnagyobb anyagvizsgáló cégénél
+            szeretnél dolgozni és kipróbálnád magad akár külföldön is akkor a
+            Volvid Kft. számodra remek lehetőség.
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <p>
+            {" "}
+            Van már középfokú végzettséged és érdekel az anyagvizsgálat?{" "}
+            <ul>
+              Mi gondoskodunk a képzésedről és hosszútávon munkalehetőséget,
+              folyamatos továbbképzést biztosítunk.
+            </ul>
+          </p>
+          <img />
+        </div>
+        <div className="flex justify-between">
+          {" "}
+          <p>Neked a jó hozzáállást és a tanulási kedvet kell hoznod.</p>
+          <img />
+        </div>
+        <div className="flex justify-between">
+          <p>
+            {" "}
+            Amennyiben már anyagvizsgáló vagy, rendelkezel érvényes papírokkal
+            akkor akár hosszú távú külföldi munkát is tudunk biztosítani
+            számodra Németországban és Ausztriában.
+          </p>
+          <img />
+        </div>
       </div>
     </section>
   );
--- app/de/Work.tsx
+++ app/de/Work.tsx
@@ -2,21 +2,47 @@ export const Work = () => {
   return (
     <section className="flex flex-col items-center gap-4">
       <p className="text-2xl font-bold">Work with Us!</p>
-      <div className="card flex w-full flex-col p-5 text-xl md:w-1/2">
-        <p>
-          We constantly have career opportunities at our company. Send an{" "}
-          <a
-            className="hover:underline"
-            href="mailto:volvid.prescherv@volvid.hu"
-          >
-            email
-          </a>{" "}
-          or contact us{" "}
-          <a href="tel:+36 20 403 5353" className="hover:underline">
-            by phone
-          </a>
-          !(Prescher Viktoria)
-        </p>
+      <div className=" flex w-full flex-col gap-3 p-5 text-xl md:w-1/2">
+        <div className="flex justify-between">
+          <img />
+          <p>
+            {" "}
+            {/* Amennyiben egy nagy múltú stabil hátterű magyar cégnél szeretnél
+          dolgozni de kipróbálnád magad akár külföldön is akkor a Volvid
+          számodra remek lehetőség. */}
+            If you would like to work at one of the countrys largest material
+            testing companies and would like to try yourself abroad as well,
+            then Volvid Ltd. is a great opportunity for you.
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <p>
+            {" "}
+            Do you already have a secondary education degree and are you
+            interested in material testing?{" "}
+            <ul>
+              We take care of your training and provide long-term job
+              opportunities, as well as continuous further education.
+            </ul>
+          </p>
+          <img />
+        </div>
+        <div className="flex justify-between">
+          {" "}
+          <p>
+            You need to bring a positive attitude and a willingness to learn.
+          </p>
+          <img />
+        </div>
+        <div className="flex justify-between">
+          <p>
+            {" "}
+            If you are already a material tester and have valid documentation,
+            we can even provide you with long-term job opportunities in Germany
+            and Austria.
+          </p>
+          <img />
+        </div>
       </div>
     </section>
   );
